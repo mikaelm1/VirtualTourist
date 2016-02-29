8f6c7dbb4d6ca6f0c9f05b2db6f670d42acded8f
Mikael Mukhsikaroyan <mikaelm@Mikaels-MacBook-Pro.local>
2016-02-29 10:25:56 -0800

    Added Core Data Stack

---
+++ VirtualTourist/CoreDataStackManager.swift
@@ -0,0 +1,100 @@
+//
+//  CoreDataStackManager.swift
+//  VirtualTourist
+//
+//  Created by Mikael Mukhsikaroyan on 2/29/16.
+//  Copyright © 2016 msquared. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+private let SQLITE_FILE_NAME = "ColorCollection.sqlite"
+
+class CoreDataStackManager {
+    
+    class func sharedInstance() -> CoreDataStackManager {
+        struct Static {
+            static let instance = CoreDataStackManager()
+        }
+        return Static.instance
+    }
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+        print("Instantiating app directory")
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        print("Instantiating managed object model")
+        let modelURL = NSBundle.mainBundle().URLForResource("Model", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(SQLITE_FILE_NAME)
+        
+        var error: NSError? = nil
+        do {
+            try coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch var error1 as NSError {
+            error = error1
+            coordinator = nil
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initalize the applcation's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = "There was an error creating or loading the application's saved data."
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError(domain: "Virtual Tourist", code: 9999, userInfo: dict as [NSObject: AnyObject])
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        } catch {
+            fatalError()
+        }
+        return coordinator
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        print("Instantiating the managedObjectContext property")
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
+    func saveContext() {
+        if let context = self.managedObjectContext {
+            var error: NSError? = nil
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch let error1 as NSError {
+                    error = error1
+                    NSLog("Unresolved error \(error)")
+                }
+            }
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
